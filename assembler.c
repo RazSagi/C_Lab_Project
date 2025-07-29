f47620e4b2634e255dfed957169eb8d577a46d92
RazSagi <Razsagi4@gmail.com>
2025-07-29 16:02:05 +0300

    Created globals header and checked the assembler printing

--- assembler.c
+++ assembler.c
@@ -1,3 +1,24 @@
 //
 // Created by Raz Sagi on 29/07/2025.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "globals.h"
+int pre_process(const char *file_name);
+int first_pass(const char *file_name);
+int second_pass(const char *file_name);
+
+int main(int argc, char *argv[]){
+    int i;
+    if (argc <2) {
+        printf("Missing files");
+        return 1;
+    }
+    for (i = 1;i<argc;i++){
+        const char *name = argv[i];
+        printf("Starting to assemble:%s\n",name);
+    }
+    return 0;
+}
---
+++ globals.h
@@ -0,0 +1,10 @@
+//
+// Created by Raz Sagi on 29/07/2025.
+//
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+#define MAX_LINE_LENGTH 80
+#define LINE_BUFFER (MAX_LINE_LENGTH + 2)
+
+#endif //GLOBALS_H

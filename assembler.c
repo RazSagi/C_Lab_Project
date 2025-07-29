38efe2fdc72e0ea35c195a7729bf8a482a51f805
RazSagi <Razsagi4@gmail.com>
2025-07-29 17:23:50 +0300

    More changes to assembler

--- assembler.c
+++ assembler.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "globals.h"
+#include "helpers.h"
+
 int pre_process(const char *file_name);
 int first_pass(const char *file_name);
 int second_pass(const char *file_name);
@@ -17,8 +19,16 @@ int main(int argc, char *argv[]){
         return 1;
     }
     for (i = 1;i<argc;i++){
-        const char *name = argv[i];
-        printf("Starting to assemble:%s\n",name);
+        char *name = append_file_extension(argv[i], ".as");
+        FILE *file = fopen(name, "r");
+        if (!file) {
+            printf("Couldent open file: %s\n", name);
+        }
+        else {
+            printf("Assembeling:%s\n", name);
+            fclose(file);
+        }
+        free(name);
     }
     return 0;
 }
--- helpers.c
+++ helpers.c
@@ -1,6 +1,6 @@
-//
+/*
 // Created by Raz Sagi on 29/07/2025.
-//
+*/
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -8,9 +8,7 @@
 
 char *append_file_extension(const char *base,const char *ext) {
     char *new_name = malloc(strlen(base) + strlen(ext) + 1);
-    if (!new_name) {
-        return nulll;
-    }
+    if (!new_name) return NULL;
     strcpy(new_name, base);
     strcat(new_name, ext);
     return new_name;

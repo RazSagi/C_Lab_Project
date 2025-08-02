005c3c2b7d8575562e8151058d83e62da48f1878
Raz Sagi <Razsagi4@gmail.com>
2025-08-02 15:23:03 +0300

    added Creating new macro list method

--- core/macro_list.c
+++ core/macro_list.c
@@ -1,1 +1,24 @@
 /*  Created by razsa on 8/2/2025  */
+
+#include <stdlib.h>
+#include <string.h>
+#include "macro_list.h"
+
+typedef struct MacroNode
+{
+    char *name;
+    char *body;
+    struct MacroNode *next;
+} MacroNode;
+struct MacroList
+{
+    MacroNode *head;
+};
+
+/*Creating new Macro List*/
+MacroList *ml_create(void)
+{
+    MacroList *L=malloc(sizeof *L);
+    if (L) L->head = NULL;
+    return L;
+}
--- headers/macro_list.h
+++ headers/macro_list.h
@@ -10,4 +10,12 @@
 
 typedef struct  MacroList MacroList;
 
+MacroList* ml_create(void);/*Creating new list*/
+void ml_delete(MacroList *list);/*Delete list*/
+/*Adding new macro to the list*/
+int ml_add(MacroList *list, const char *name,const char *body);
+
+/*Looking for macro by name, returns null if not in list*/
+const char *ml_lookup(const MacroList *list, const char *name);
+
 #endif

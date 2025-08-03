eea3732821c08461730bfe1983efbb56c1e6605f
Raz Sagi <Razsagi4@gmail.com>
2025-08-03 16:06:31 +0300

    added first letter method(gets to the first char of a line)

--- core/preproc.c
+++ core/preproc.c
@@ -4,9 +4,31 @@
 #include "macro_list.h"
 #include <stdio.h>
 #include <string.h>
+#include "globals.h"
+#include <stdlib.h>
+/*This method gets a pointer to line and returns if there is "real" char there*/
+static char* first_letter(char *line)
+{
+    char *curr = line;
+    while (*curr==' '||*curr=='\t')
+    {
+        curr++;
+    }
+    if (*curr == '\0'||*curr == '\n'||*curr == '\r')
+    {
+        return NULL;
+    }
+    return curr;
+
+}
+
 
 int run_preprocessor(const char *in_path,char *out_path)
 {
+    FILE *in;
+    FILE *out;
+    MacroList *macros = ml_create();
+    char line[MAX_LINE_LENGTH]
     (void)in_path;
     (void)out_path;
     return 0;
--- headers/globals.h
+++ headers/globals.h
@@ -6,5 +6,8 @@
 #define GLOBALS_H
 #define MAX_LINE_LENGTH 80
 #define LINE_BUFFER (MAX_LINE_LENGTH + 2)
+#define PREPROC_OK 0
+#define PREPROC_ERR_OPEN_IN 1
+#define PREPROC_ERR_OPEN_OUT 2
 
 #endif

8def1379e598c2dfce3f19cf91a1fbd767acbbdc
Raz Sagi <Razsagi4@gmail.com>
2025-08-08 18:13:51 +0300

    added symbol table handling

---
+++ core/sym_table.c
@@ -0,0 +1,57 @@
+/*  Created by razsa on 8/8/2025  */
+
+
+#include "sym_table.h"
+#include<string.h>
+#include<stdio.h>
+#include "globals.h"
+
+static Symbol g_symbols[SYM_TABLE_MAX];
+static int g_sym_count = 0;
+
+void sym_table_reset(void)
+{
+    g_sym_count = 0;
+}
+
+static int name_equals(const char *name1, const char *name2)
+{
+    return strcmp(name1, name2) == 0;
+}
+
+int sym_table_contains(const char *name)
+{
+    int i;
+    for (i = 0; i < g_sym_count; i++)
+    {
+        if (name_equals(g_symbols[i].name, name))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+/*adding to the symbols table*/
+int sym_table_add(const char *name, int value, SymKind kind)
+{
+    size_t length;
+    /*dupe symbol*/
+    if (sym_table_contains(name))
+    {
+        return 0;
+    }
+    /*too many symbols*/
+    if (g_sym_count >= SYM_TABLE_MAX)
+    {
+        return 0;
+    }
+
+    length = strlen(name);
+    memcpy(g_symbols[g_sym_count].name, name, length+1);
+
+    g_symbols[g_sym_count].value = value;
+    g_symbols[g_sym_count].kind = kind;
+
+    g_sym_count++;
+    return 1;
+}
---
+++ headers/sym_table.h
@@ -0,0 +1,33 @@
+/*  Created by razsa on 8/8/2025  */
+
+
+#ifndef SYM_TABLE_H
+#define SYM_TABLE_H
+
+#include "globals.h"
+
+typedef enum
+{
+    SYM_CODE = 1,
+    SYM_DATA = 2,
+    SYM_EXTERN = 3,
+    SYM_ENTRY = 4
+}SymKind;
+
+typedef struct
+{
+    char name[MAX_LABEL_LENGTH];
+    int value;
+    SymKind kind;
+}Symbol;
+
+/*reset the table between files*/
+void sym_table_reset(void);
+
+/*return 1 if the label is laready exists*/
+int sym_table_contains(const char *name);
+
+/*return 1 on success adding, 0 if dupe or table full*/
+int sym_table_add(const char *name,int value,SymKind kind);
+
+#endif
